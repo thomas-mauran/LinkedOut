d95bb489a6dded5930ced1997641a095900884c6
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-10-13 12:39:21 +0200

    feat: configure spring security with keycloak

---
+++ backend/src/main/java/com/linkedout/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package com.linkedout.backend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class BackendApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BackendApplication::class.java, *args)
+}
---
+++ backend/src/main/java/com/linkedout/backend/config/SecurityConfig.kt
@@ -0,0 +1,39 @@
+package com.linkedout.backend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+internal open class SecurityConfig {
+    @Bean
+    @Throws(Exception::class)
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.oauth2ResourceServer { auth -> auth.jwt { } }
+            .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { auth -> auth.anyRequest().authenticated() }
+
+        return http.build()
+    }
+
+    @Bean
+    open fun jwtAuthenticationConverter(): JwtAuthenticationConverter {
+        val jwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter()
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("platform_roles")
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_")
+
+        val jwtAuthenticationConverter = JwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter)
+        return jwtAuthenticationConverter
+    }
+}
