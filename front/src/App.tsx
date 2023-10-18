cdf71fc672df718d08bd8d3241c2cbe13143f5a3
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2023-10-19 00:00:41 +0200

    feat: change color palette of mobile app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,71 +1,16 @@
-import {
-  NavigationContainer,
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationDefaultTheme,
-} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import deepmerge from 'deepmerge';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useCallback } from 'react';
-import { Platform, useColorScheme } from 'react-native';
-import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  MD3TypescaleKey,
-  PaperProvider,
-  adaptNavigationTheme,
-  configureFonts,
-} from 'react-native-paper';
-import { MD3Type } from 'react-native-paper/lib/typescript/types';
+import { useColorScheme } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 
 import InternalTabNav from '@/pages/InternalTabNav';
 import store from '@/store/Store';
-
-// Theme setup
-const { LightTheme, DarkTheme } = adaptNavigationTheme({
-  reactNavigationLight: NavigationDefaultTheme,
-  reactNavigationDark: NavigationDarkTheme,
-});
-
-const DefaultFontHeading = Platform.select({
-  web: 'RedHatDisplay',
-  ios: 'RedHatDisplay',
-  default: 'RedHatDisplay',
-});
-
-const FontConfig: Partial<Record<MD3TypescaleKey, Partial<MD3Type>>> = {
-  displayLarge: {
-    fontFamily: DefaultFontHeading,
-  },
-  displayMedium: {
-    fontFamily: DefaultFontHeading,
-  },
-  displaySmall: {
-    fontFamily: DefaultFontHeading,
-  },
-  headlineLarge: {
-    fontFamily: DefaultFontHeading,
-  },
-  headlineMedium: {
-    fontFamily: DefaultFontHeading,
-  },
-  headlineSmall: {
-    fontFamily: DefaultFontHeading,
-  },
-};
-
-const CombinedDefaultTheme = deepmerge(LightTheme, {
-  ...MD3LightTheme,
-  fonts: configureFonts({ config: FontConfig }),
-});
-
-const CombinedDarkTheme = deepmerge(DarkTheme, {
-  ...MD3DarkTheme,
-  fonts: configureFonts({ config: FontConfig }),
-});
+import { DarkTheme, LightTheme } from '@/utils/theme';
 
 // We want to hide the splash screen ourselves
 SplashScreen.preventAutoHideAsync();
@@ -86,8 +31,7 @@ const RootStack = createNativeStackNavigator<RootStackParamList>();
 const App = () => {
   // Switch the color scheme of the application based on the system-wide setting
   const colorScheme = useColorScheme();
-  const theme =
-    colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+  const theme = colorScheme === 'dark' ? DarkTheme : LightTheme;
 
   // Load application fonts
   const [fontsLoaded, fontError] = useFonts({
