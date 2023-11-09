9f83b9814a394654782bf3650a8cddee07f88494
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2023-11-09 18:26:06 +0100

    chore(backend): set up a Gradle multi-project build

--- backend/api_gateway/build.gradle.kts
+++ backend/api_gateway/build.gradle.kts
@@ -21,17 +21,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator:3.1.5")
-	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc:3.1.5")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.1.5")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.1.5")
 	implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
-	implementation("org.springframework.boot:spring-boot-starter-webflux:3.1.5")
 	implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
-	implementation("org.springframework:spring-jdbc:6.0.13")
-	implementation("org.flywaydb:flyway-core:9.22.3")
-	implementation("org.postgresql:r2dbc-postgresql:1.0.2.RELEASE")
-	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.20")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ backend/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.springframework.boot") version "3.1.5" apply false
+    id("io.spring.dependency-management") version "1.1.3" apply false
+    kotlin("jvm") version "1.9.20" apply false
+    kotlin("plugin.spring") version "1.9.20" apply false
+    kotlin("plugin.jpa") version "1.9.20" apply false
+}
+
+rootProject.name = "linkedout-backend"
+include("api_gateway")
+include("jobs")
