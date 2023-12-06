6ff37217f9a198264f2a2db513ac351de371bbdc
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-12-06 10:34:27 +0100

    feat(backend): bootstrap messaging service (#27)

---
+++ backend/messaging/src/test/kotlin/com/linkedout/jobs/MessagingApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.linkedout.jobs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessagingApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
