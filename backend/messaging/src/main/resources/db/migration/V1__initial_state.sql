5cd3780252dcbfaefe0004f4758e3a8cc6938eb9
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-12-10 18:34:57 +0100

    feat(messaging): ensure only one channel per seasonworker and employer

--- backend/messaging/src/main/resources/db/migration/V1__initial_state.sql
+++ backend/messaging/src/main/resources/db/migration/V1__initial_state.sql
@@ -1,5 +1,6 @@
 CREATE TABLE MessageChannel (
                                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                                 seasonworkerId UUID NOT NULL,
-                                employerId UUID NOT NULL
+                                employerId UUID NOT NULL,
+                                CONSTRAINT u_message_channel UNIQUE (seasonworkerId, employerId)
 );
