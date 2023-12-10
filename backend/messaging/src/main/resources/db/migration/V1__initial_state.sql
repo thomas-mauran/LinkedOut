adebf4bbe6d0ee17a1fda84bed824404b8a292ac
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2023-12-10 20:31:43 +0100

    chore(messaging): move DB constraint modification to its own migration file

--- backend/messaging/src/main/resources/db/migration/V1__initial_state.sql
+++ backend/messaging/src/main/resources/db/migration/V1__initial_state.sql
@@ -1,6 +1,5 @@
 CREATE TABLE MessageChannel (
                                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                                 seasonworkerId UUID NOT NULL,
-                                employerId UUID NOT NULL,
-                                CONSTRAINT u_message_channel UNIQUE (seasonworkerId, employerId)
+                                employerId UUID NOT NULL
 );
