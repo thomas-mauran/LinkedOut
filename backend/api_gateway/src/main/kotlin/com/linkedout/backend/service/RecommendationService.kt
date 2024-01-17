ddf2b74452198c511be51d36b72d20b6f844b96f
Mauran <thomas.mauran@etu.umontpellier.fr>
2024-01-17 07:25:10 +0100

    chore: lint
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- backend/api_gateway/src/main/kotlin/com/linkedout/backend/service/RecommendationService.kt
+++ backend/api_gateway/src/main/kotlin/com/linkedout/backend/service/RecommendationService.kt
@@ -1,16 +1,11 @@
 package com.linkedout.backend.service
 
-import com.linkedout.backend.model.Company
-import com.linkedout.backend.model.Job
-import com.linkedout.backend.model.JobOffer
 import com.linkedout.backend.model.Recommendation
 import com.linkedout.common.service.NatsService
 import com.linkedout.common.utils.RequestResponseFactory
-import com.linkedout.proto.models.JobOfferOuterClass
 import com.linkedout.proto.services.Recommendations
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 
 @Service
 class RecommendationService(
@@ -20,7 +15,7 @@ class RecommendationService(
     fun findAll(requestId: String): List<Recommendation> {
         // Request job offers from the job service
         val request = RequestResponseFactory.newRequest(requestId)
-            .setGetUserRecommendation(
+            .setGetRecommendationRequest(
                 Recommendations.GetRecommendationRequest.newBuilder()
             )
             .build()
