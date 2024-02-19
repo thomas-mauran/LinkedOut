1083379c70638bdbe7093e2c35ce8fa82bda26ae
Kuruyia <8174691+Kuruyia@users.noreply.github.com>
2024-02-19 11:19:06 +0100

    feat(front): decode the JWT to get admin status

--- front/src/store/features/authenticationSlice.ts
+++ front/src/store/features/authenticationSlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import * as SecureStore from 'expo-secure-store';
+import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 /**
  * The key used to store the authentication state in the secure store.
@@ -19,6 +20,7 @@ type UnknownSliceState = {
 type AuthenticatedSliceState = {
   state: 'authenticated';
   token: string;
+  isAdmin: boolean;
 };
 
 /**
@@ -37,6 +39,34 @@ type SliceState =
   | UnauthenticatedSliceState;
 
 /**
+ * The role name for an admin.
+ */
+const ADMIN_ROLE = 'client_admin';
+
+/**
+ * The contents of the JWT token.
+ */
+type TokenContent = {
+  /**
+   * The roles of the user.
+   */
+  platform_roles: string[];
+};
+
+/**
+ * Updates the state with the token.
+ */
+const updateStateWithToken = (
+  state: AuthenticatedSliceState,
+  token: string,
+) => {
+  const decodedToken = jwtDecode<TokenContent & JwtPayload>(token);
+
+  state.token = token;
+  state.isAdmin = decodedToken.platform_roles.includes(ADMIN_ROLE);
+};
+
+/**
  * The authentication slice that contains info about whether the user is authenticated.
  */
 const slice = createSlice({
@@ -45,16 +75,17 @@ const slice = createSlice({
   reducers: {
     init: (state) => {
       const token = SecureStore.getItem(SECURE_STORE_TOKEN_KEY);
-
       state.state = token ? 'authenticated' : 'unauthenticated';
+
       if (state.state === 'authenticated') {
-        state.token = token;
+        updateStateWithToken(state, token);
       }
     },
     login: (state, action: PayloadAction<string>) => {
       state.state = 'authenticated';
+
       if (state.state === 'authenticated') {
-        state.token = action.payload;
+        updateStateWithToken(state, action.payload);
       }
 
       SecureStore.setItem(SECURE_STORE_TOKEN_KEY, action.payload);
